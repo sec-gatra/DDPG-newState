c43409ba50287268a3c8736c168067c93e69f934
sec-gatra <akhiragatra4@gmail.com>
2025-07-10 09:22:27 +0700

    Update utils20.py

--- utils20.py
+++ utils20.py
@@ -111,7 +111,7 @@ def evaluate_policy_reward(channel_gain, state, env, agent, turns=3):
             a = agent.select_action(state, deterministic=True)
             next_loc = env.generate_positions()
             next_channel_gain= env.generate_channel_gain(next_loc)
-            s_next, re, dw, tr, info = env.step(a, channel_gain, next_channel_gain)
+            s_next, re, dw, tr, info = env.step(a, channel_gain)
             #if iterasi == max_iter :
             #    tr ==True
             done = (dw or tr)
